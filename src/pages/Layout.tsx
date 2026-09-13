a28fba69eeba5617bd0a685839267c9e765df070
myothiha97 <47525641+myothiha97@users.noreply.github.com>
2026-09-13 22:24:37 +0700

    chore: remove dead codes

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,31 +1,24 @@
-import { ReactNode, useContext, useState } from "react";
-import Navbar from "../component/Navbar";
-import Footer from "../component/Footer";
-import { AuthContext, AuthContextWrapper } from "@/context/auth/AuthContext";
-import useAuth from "@/hooks/useAuth";
+import { ReactNode } from 'react'
+import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
+import useAuth from '@/hooks/useAuth'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  // const authContext = useContext(AuthContext);
-  // const isLogin = Boolean(authContext?.profile);
+  const auth = useAuth()
 
-  // console.log({ authContext, isLogin });
-
-  const auth = useAuth();
-
-  console.log({ auth });
   return (
-    <div className="min-h-screen flex flex-col gap-10">
+    <div className='min-h-screen flex flex-col gap-10'>
       {auth.isLogin && <Navbar />}
       <main>{children}</main>
 
-      <button
-        className="text-white"
-        onClick={() => auth.setProfile({ email: "hein" })}
-      >
-        Login
-      </button>
+      {/* <button */}
+      {/*   className="text-white" */}
+      {/*   onClick={() => auth.setProfile({ email: "hein" })} */}
+      {/* > */}
+      {/*   Login */}
+      {/* </button> */}
 
       {auth.isLogin && <Footer />}
     </div>
-  );
+  )
 }
